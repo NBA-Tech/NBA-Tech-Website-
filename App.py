6606319e3524e633ae25d7a80fc393c1199c181e
ra-lgt <raviajay9344@gmail.com>
2024-03-16 09:50:45 +0530

    bug

--- App.py
+++ App.py
@@ -19,14 +19,14 @@ def services():
     with open('Data/services.json', 'r') as f:
         data = json.load(f)
     return render_template('services_prod.html',service_data={"services":data,"type":service_type})
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404  
+
 @app.route('/gallery')
 def gallery():
     with open('Data/project_details.json', 'r') as f:
         data = json.load(f)
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404  
-    
     return render_template('gallery.html',project_data={"project":data,"length":len(data)})
 if __name__ == '__main__':
     app.run(debug=True)
