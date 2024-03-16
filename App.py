8cda971d9f687f66789b6a7d3f10d80212218d01
ra-lgt <raviajay9344@gmail.com>
2024-03-16 10:49:35 +0530

    contact us and navbar clean up

--- App.py
+++ App.py
@@ -28,5 +28,9 @@ def gallery():
     with open('Data/project_details.json', 'r') as f:
         data = json.load(f)
     return render_template('gallery.html',project_data={"project":data,"length":len(data)})
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 if __name__ == '__main__':
     app.run(debug=True)
